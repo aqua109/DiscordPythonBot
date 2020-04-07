065d570625fadb378b46384a3bab0e857f1dde86
Aidan <aqua109@aucklanduni.ac.nz>
2020-04-07 20:12:49 +1200

    b r u h

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ command_dict = \
     {
         "aoe2": ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42"],
         "quake": ["dominating", "godlike", "holyshit", "humiliation", "killingspree", "ludicrouskill", "monsterkill", "rampage", "teamkiller", "ultrakill", "unstoppable", "wickedsick"],
-        "misc": ["how", "disgusting", "penis", "foodplease", "dialup", "aeugh", "brrringha", "idiotsandwich", "bruh", "supersuit", "quickscope", "petruh"],
+        "misc": ["how", "disgusting", "penis", "foodplease", "dialup", "aeugh", "brrringha", "idiotsandwich", "bruh", "supersuit", "quickscope", "petruh", "firerate", "bustlingfungus", "ohno", "ahh"],
         "help": ["help", "disconnect", "aoe2help", "quakehelp", "mischelp"]
     }
 
